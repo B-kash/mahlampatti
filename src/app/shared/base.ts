1795416b3dbdad2590c94399d88e9d9c9402733c
b-kash <bikashchapagain047@gmail.com>
2018-11-29 00:48:47 +0545

    page created

---
+++ src/app/shared/base.ts
@@ -0,0 +1,12 @@
+export class Base {
+  id: number;
+  addedBy: number;
+  addedOn: Date;
+  modifiedBy: number;
+  modifiedOn: Date;
+  deletedBy: number;
+  deletedOn: Date;
+
+  constructor() {
+  }
+}
---
+++ src/app/shared/user.ts
@@ -0,0 +1,13 @@
+import {Base} from './base';
+
+export class User extends Base {
+  name: string;
+  email: string;
+  userName: string;
+  password: string;
+
+  constructor() {
+    super();
+
+  }
+}
