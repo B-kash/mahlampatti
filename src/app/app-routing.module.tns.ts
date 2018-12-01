2aa27e4f4289ddddb1e734bdb82f0d27ae8b66ea
b-kash <bikashchapagain047@gmail.com>
2018-12-02 00:32:42 +0545

    admin page side drawaer done

--- src/app/app-routing.module.tns.ts
+++ src/app/app-routing.module.tns.ts
@@ -3,10 +3,12 @@ import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
 
 import {LoginComponent} from './login/login.component';
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
