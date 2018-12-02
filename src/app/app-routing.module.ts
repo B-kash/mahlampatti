f47dead3af48b1451d58141a964bda754eb87b64
Bikash Chapagain <bikash.chapagain@yco.com.np>
2018-12-02 10:31:31 +0545

    routing changed according to need

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
-import {HomeComponent} from "./home/home.component";
-import {PatientListComponent} from "./patient-list/patient-list.component";
+
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'patients', component: PatientListComponent},
+  // {path: 'home', component: HomeComponent},
+  // {path: 'patients', component: PatientListComponent},
 
 ];
 
