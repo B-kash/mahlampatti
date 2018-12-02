e47de7421fbbd1b0687e2e09569d480081ac8370
Bikash Chapagain <bikash.chapagain@yco.com.np>
2018-12-02 10:26:47 +0545

    login page changes, register page changes, changes on css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {HomeComponent} from "./home/home.component";
+import {PatientListComponent} from "./patient-list/patient-list.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'patients', component: PatientListComponent},
+
 ];
 
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userName:string;
+  password:string;
   isRegisterPage:boolean = false;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  login(){
+    //CHALLANGE  is just a object used to pass username and password to the backed
+    let challange = {
+      userName: this.userName,
+      password: this.password
+    }
+  //  TODO call api here with the above challange
+
+  //  after calling the api route it to the home page
+    this.router.navigate(['/home']);
+
+  }
 }
